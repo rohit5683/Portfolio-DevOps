4999c99f604b972ae84ecefe1a4bb8241f476e6a
Rohit <killerrohitrocks007@gmail.com>
2026-04-03 12:12:34 +0530

    chore: remove all splash screens for a pure instant-on experience

--- next-app/src/app/providers.tsx
+++ next-app/src/app/providers.tsx
@@ -4,25 +4,10 @@ import { ThemeProvider } from "next-themes";
 import { AuthProvider } from "@/context/AuthContext";
 import SmoothScroll from "@/components/common/SmoothScroll";
 import CustomCursor from "@/components/common/CustomCursor";
-import SplashScreen from "@/components/common/SplashScreen";
-import { AnimatePresence } from "framer-motion";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const [showSplash, setShowSplash] = React.useState(true);
-  const [isMounted, setIsMounted] = React.useState(false);
-
-  React.useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
-      <AnimatePresence mode="wait">
-        {showSplash && isMounted && (
-          <SplashScreen key="splash" onFinish={() => setShowSplash(false)} />
-        )}
-      </AnimatePresence>
-
       <SmoothScroll>
         <CustomCursor />
         <AuthProvider>
